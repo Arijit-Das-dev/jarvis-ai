afaf658c9df8aad919e99032c25757b1cb0658c9
Arijit Das <arijitdas.das99@gmail.com>
2026-05-24 10:28:52 +0530

    updated page navigation

--- Backend/Features/app.py
+++ Backend/Features/app.py
@@ -5,15 +5,45 @@ style1()
 # -------------------------------
 # NAVIGATION
 # -------------------------------
-account_page = st.Page("account.py", title="Account", icon=":material/account_circle:")
-home_page = st.Page("home.py", title="About", icon=":material/home:", default=True)
-main_page = st.Page("main.py", title="Assistant", icon=":material/smart_toy:")
-code_page = st.Page("CodeEditor.py", title="Code/Debug", icon=":material/code:")
-image_page = st.Page("Image.py", title="ImageLab", icon=":material/image:")
-prompt_eng_page = st.Page("PromptEng.py", title="PromptLab", icon=":material/edit_note:")
-Jarvis_scholar_page = st.Page("Scholar.py", title="Scholar", icon=":material/search:")
-jarvis_resume = st.Page("Resume.py", title="Resume", icon=":material/draft:")
-pages = [account_page,home_page, main_page, code_page, image_page, prompt_eng_page, Jarvis_scholar_page, jarvis_resume]
 
+# -------------------------------
+# FEATURE GROUP
+# -------------------------------
+features = [
+    st.Page("main.py", title="Assistant", icon=":material/smart_toy:"),
+    st.Page("CodeEditor.py", title="Code/Debug", icon=":material/code:"),
+    st.Page("Image.py", title="ImageLab", icon=":material/image:"),
+    st.Page("PromptEng.py", title="PromptLab", icon=":material/edit_note:"),
+    st.Page("Scholar.py", title="Jarvis Scholar", icon=":material/search:"),
+    st.Page("Resume.py", title="Jarvis Resume", icon=":material/draft:")
+]
+
+# -------------------------------
+# MAIN NAVIGATION
+# -------------------------------
+pages = {
+    "Account": [
+        st.Page(
+            "account.py",
+            title="Account",
+            icon=":material/account_circle:"
+        )
+    ],
+
+    "About": [
+        st.Page(
+            "home.py",
+            title="About",
+            icon=":material/home:",
+            default=True
+        )
+    ],
+
+    "Features": features
+}
+
+# -------------------------------
+# RUN APP
+# -------------------------------
 app = st.navigation(pages)
 app.run()
